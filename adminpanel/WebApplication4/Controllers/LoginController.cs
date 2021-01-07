07c7c2d42cec0eea5f6df8c087bb045eb7ada579
ranafarhanayub4 <Chota7184493>
2021-01-07 17:12:11 +0500

    first commit

---
+++ adminpanel/WebApplication4/Controllers/LoginController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication4.Models;
+
+namespace WebApplication4.Controllers
+{
+    public class LoginController : Controller
+    {
+        ManagmentSystemEntities db = new ManagmentSystemEntities();
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(string email,string password)
+        {
+            var user = db.Users.FirstOrDefault(x=>x.email.Equals(email) && x.password.Equals(password));
+            if(user!=null)
+            {
+                Session["userid"] = user.id;
+                Session["username"] = user.name;
+                Session["useremail"] = user.email;
+                return RedirectToAction("Index","Admin");
+
+            }
+            else
+            {
+                return RedirectToAction("Index");
+            }
+            
+        }
+    }
+}
---
+++ adminpanel/WebApplication4/Controllers/adminController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication4.Controllers
+{
+    public class adminController : Controller
+    {
+        // GET: admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Logout()
+        {
+            Session.Clear();
+            return RedirectToAction("Index","Login");
+        }
+    }
+}
